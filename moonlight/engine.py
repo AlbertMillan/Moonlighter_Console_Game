818705db765512370e50976ef6897407e5ba30fe
Albert Millán <scyam1@nottingham.edu.cn>
2018-06-09 16:38:20 +0200

    Generate Map using tree data structure

--- moonlight/engine.py
+++ moonlight/engine.py
@@ -1,45 +1,89 @@
-from random import randint, choice
+from random import randint, choice, shuffle
 
 enemies = ['weak', 'moderate', 'strong']
-children = [None, 1, 2]
+children = [1, 2]
 
 class Map(object):    # can be level
     
     def __init__(self, size, max_connections ):
         self.scenes = []
-        arr = []
+        self.arr = []
+        self.closed = []
         
-        #Create scenes given by size
-        for i in size:
-            self.scenes.append(Scene(i, randint(0, 4), randint(-1, 1)))
-            arr.append(i)
+        # Create scenes given the size of the map
+        for i in range(size):
+            # Create scenes
+            newScene = Scene(i, randint(0, 4), randint(-1, 1) )
+            self.scenes.append(newScene)
 
-        # 'Connect' each scene with others in order to create a map
-        self.scenes[0].set_connection(1, 2, 3)
+            # Contains the id of each scenes. Scenes in this list have not yet been 'connected' to other scenes.
+            self.arr.append(i)
+            
+            if i < 2:
+                self.closed.append(True)             # At least two nodes/scenes must be closed (have no child nodes)
+            else:
+                self.closed.append(False)            # All others can be open (have child nodes)
+        
+        # Adjust for: First element is manually 'connected' with others.
+        self.arr.pop(0)          # Node 0
+        self.closed.pop()
+        self.closed.pop()
+        shuffle(self.closed)
 
-        # remove them from the possible connections. A child cannot be connected with other same level nodes on the tree
-        arr.pop(0)
-        arr.pop(0)
-        arr.pop(0)
-        arr.pop(0)
+    # 'Connect' each scene with others in order to create a map. Remove them from the possible connections. 
+    # A child cannot be connected with other same level nodes on the tree
+        # Root node has initially 3 children
+        self.scenes[0].set_connection( self.scenes[self.arr.pop(0)] )     # Node 1
+        self.scenes[0].set_connection( self.scenes[self.arr.pop(0)] )     # Node 2
+        self.scenes[0].set_connection( self.scenes[self.arr.pop(0)] )     # Node 3
 
-        for i in size-1:
-            # set the number of connections of the current node
-            scene_connections = choice(children)
 
+        # Generate the 'connections' of the remaining nodes.
+        self.generate_tree_structure(size)
+
+
+    def generate_tree_structure(self, size):
+        for i in range(size-2):
+            print( "\n", self.arr, "\n", self.closed )
+
+            # Set the number of connections of the current node
+            scene_connections = self.get_children()
+
+            # Current node has one child (2 connections: parent + child)
             if scene_connections == 1:
-                #self.scenes[i+1].set_connection[i+2]
-                self.scenes[i+1].set_connection( arr.pop(0), None )
+                self.scenes[i+1].set_connection( self.scenes[self.arr.pop(0)] )
 
+            # Current node has two child nodes (3 connections: parent + 2 * child)
             elif scene_connections == 2:
-                #self.scenes[i+1].set_connection[i+2]
-                #self.scenes[i+1].set_connection[i+3]
-                self.scenes[i+1].set_connection( arr.pop(0), arr.pop(1) )
+                self.scenes[i+1].set_connection( self.scenes[self.arr.pop(0)] )
+                self.scenes[i+1].set_connection( self.scenes[self.arr.pop(0)] )
+
+                # Two children means an extra path, which must be closed at some point. Keep the same number of nodes.
+                swap = False
+                j = 0
+                while not swap:
+                    if self.closed[j] == False:
+                        self.closed[j] = True
+                        swap = True
+                    j = j + 1
 
             else:
-                print("Scene %d has no doors to other rooms" % i)
+                print("Scene %d has no doors to other rooms" % (i+1) ) 
 
+        print("Loop Completed!")
 
+    # Probabilistic determination of the number of children of given node (none, 1, or 2)
+    def get_children(self):
+        pos = randint( 0, len(self.closed) ) - 1    # Initially, two nodes must be closed (have no children)
+
+        if self.closed[pos]   ==   False:
+            scene_connections = choice(children)
+        else:
+            scene_connections = None
+        
+        self.closed.pop(pos)
+
+        return scene_connections
 
 
 
@@ -79,3 +123,6 @@ class Enemy(Character):
     def __init__(self, health, attack_damage):
         super(Enemy, self).__init__(health, attack_damage)
         self.type = choice(enemies)
+
+
+myMap = Map(12, 3)
